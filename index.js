0172a1bf712929e0e34f42c4a227685f0f1c1a7e
Jatin Vaidya <jatin.vaidya@gmail.com>
2022-05-11 14:42:19 +1000

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const { auth, requiresAuth } = require("express-openid-connect");
+const fetch = require("node-fetch");
+require("dotenv").config();
+
+const config = {
+	authRequired: false,
+	auth0Logout: true,
+	baseURL: process.env.BASE_URL,
+	clientID: process.env.CLIENT_ID,
+	issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
+	secret: process.env.COOKIE_SECRET,
+};
+
+const app = express();
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+	res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+// enforce authentication and post id_token back
+app.get("/secure", requiresAuth(), async (req, res) => {
+	console.log(`Redirect State: ${req.query.state}`);
+	const params = new URLSearchParams();
+	params.append("id_token", req.oidc.idToken);
+	const response = await fetch(
+		`https://${process.env.AUTH0_DOMAIN}/continue?state=${req.query.state}`,
+		{
+			method: "POST",
+			body: params,
+		}
+	);
+});
+
+app.listen(3000, () => {
+	console.log(`Example app listening on port 3000`);
+});
