858a3392575e54d0c8199a72b4045cc46127b2e0
Jatin Vaidya <jatin.vaidya@gmail.com>
2022-05-12 11:21:20 +1000

    fix view pat

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(auth(config));
 
 // templating
 app.set("view engine", "pug");
-app.set("views", "./views");
+app.set("views", path.join(__dirname, ".", "views"));
 
 // modify requiresAuth to support query params on Authorization Request
 const requiresAuthWithParam = (req, res, next) => {
@@ -40,7 +40,7 @@ app.get("/", (req, res) => {
 
 // enforce authentication and post id_token back
 app.get("/secure", requiresAuthWithParam, (req, res) => {
-	res.render("form-post.pug", {
+	res.render("form-post", {
 		continue_endpoint: `https://${process.env.AUTH0_DOMAIN}/continue?state=${req.query.state}`,
 		id_token: req.oidc.idToken,
 	});
